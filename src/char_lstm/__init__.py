06439c14f956819932125bbe18364c47191b8a2c
Austin Wagner <austinwagner@msn.com>
2025-12-02 21:46:22 -0800

    refactor: Rename charLSTM to CharLSTM for PEP 8 compliance

--- src/char_lstm/__init__.py
+++ src/char_lstm/__init__.py
@@ -1,11 +1,11 @@
 """Character-level LSTM for Turkic language modeling."""
 
 from char_lstm.data import CharDataset, build_vocab_with_unk, load_vocab_json, save_vocab_json
-from char_lstm.model import charLSTM
+from char_lstm.model import CharLSTM
 
 __all__ = [
-    "charLSTM",
     "CharDataset",
+    "CharLSTM",
     "build_vocab_with_unk",
     "load_vocab_json",
     "save_vocab_json",
